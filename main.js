ab73970c10f5414581264d2993a902b39b5bb95e
JCuzzy1 <cousins.joseph@googlemail.com>
2024-08-25 21:54:14 +0100

    I get an NaN message when I click on the equals button. Fixed this by adding an if condition on the equals onClick eventlistner function. If current value is not empty and previous value is not empty, allow function to run.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /* First things I want my calculator to do:
 
 - When the user clicks a number or operator button,the display should update with the value
-of the button.
+of the button but also display the previous value.
 
 - When the clear button is clicked, the display should reset/clear.
 
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const previousScreen = document.querySelector(".previous");
     const currentScreen = document.querySelector(".current");
 
-// add an eventListener to each of the number buttons
+    // add an eventListener to each of the number buttons
 
     numbers.forEach((number) => number.addEventListener("click", function(e) {
         handleNumber(e.target.textContent)
@@ -34,11 +34,30 @@ document.addEventListener("DOMContentLoaded", function() {
         currentScreen.textContent = currentValue; 
     }))
 
-// add an eventListener to each of the operator buttons
+    // add an eventListener to each of the operator buttons
 
-operators.forEach((op) => op.addEventListener("click", function(e) {
-    handleOperator(e.target.textContent)
+    operators.forEach((op) => op.addEventListener("click", function(e) {
+        handleOperator(e.target.textContent)
+        previousScreen.textContent = previousValue + "" + operator;
+        currentScreen.textContent = currentValue;
     }))
+
+    // add an eventListener to the clear button
+
+    clear.addEventListener("click", function() {
+        previousValue = '';
+        currentValue = '';
+        operator = '';
+        previousScreen.textContent = currentValue;
+        currentScreen.textContent = currentValue;
+    })
+
+    // add an eventListener to the equals button. Get it to calculate basic equations.
+
+    equals.addEventListener("click", function() {
+       calculate() 
+    })
+
 })
 
 function handleNumber(num) {
@@ -48,7 +67,35 @@ function handleNumber(num) {
 }
 
 function handleOperator(op) {
-    console.log(op);
+    operator = op;
+    previousValue = currentValue;
+    currentValue = '';
+}
+
+function calculate() {
+    previousValue = Number(previousValue);
+    currentValue = Number(currentValue);
+
+    if(operator === "+") {
+        previousValue += currentValue;
+    } else if(operator === "-") {
+        previousValue -= currentValue;
+    } else if(operator === "X") {
+        previousValue *= currentValue;
+    } else {
+        previousValue /= currentValue;
+    }
+
+    previousValue = roundNumber(previousValue);
+    previousValue= previousValue.toString();
+    currentValue = previousValue.toString();
+    // console.log(previousValue);
+}
+
+// round result to 15 digits
+
+function roundNumber(num) {
+    return Math.round(num * 100000000000000) / 100000000000000;
 }
 
 
